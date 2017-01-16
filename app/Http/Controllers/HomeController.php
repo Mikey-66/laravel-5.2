0005ab13c4efd70839b6e0a2ea7a9f6992934326
Mikey-66 <805742791@qq.com>
2017-01-16 22:21:23 +0800

    'd'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Models\Article;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home')->withArticles(\App\Article::all());
+        return view('home')->withArticles(Article::all());
+    }
+    
+    public function show(){
+        dd('i love laravel');
     }
 }
